2f237bcb12a81083acf49d20a75ab5977a4f5488
MalgAmoe <malgamoe@gmail.com>
2017-05-21 00:25:16 +0200

    feat: mapping over questions received

--- client/src/ExplorationContainer/Components/Category.js
+++ client/src/ExplorationContainer/Components/Category.js
@@ -2,25 +2,31 @@ import React, { Component } from 'react'
 import { withRouter } from 'react-router-dom'
 
 class Category extends Component {
-  componentWillMount(){
-    let cat
-    if(this.props.match.params.category === 'nature') {
-      cat = 1
-    } else if (this.props.match.params.category === 'science') {
-      cat = 2
-    } else if (this.props.match.params.category === 'humanities') {
-      cat = 3
-    }
-    fetch(`http://localhost:3000/questions/cat/${cat}`)
+  constructor(props){
+    super(props)
+    this.state = {questions: []}
+  }
+  componentDidMount(){
+
+    fetch(`http://localhost:3000/questions/cat/${this.props.match.params.category}`)
       .then(res => res.json())
       .then(questions => {
-        console.log(questions);
+        if (questions) {
+          this.setState({questions: questions})
+          console.log(questions);
+        }
       })
 
   }
   render() {
     return(
-      <div>yayayay</div>
+      <div>
+        {
+          this.state.questions.map((question, i) => {
+            return <div key={i}>{question.body}</div>
+          })
+        }
+      </div>
     )
   }
 }
