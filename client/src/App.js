e171a73582760dd0525426037eedab105ff44c2d
MalgAmoe <malgamoe@gmail.com>
2017-05-20 18:39:25 +0200

    feat: input box

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,29 @@
 import React, { Component } from 'react';
 import Header from './Header'
+import QuestionContainer from './QuestionContainer'
+import ExplorationContainer from './ExplorationContainer'
 import './App.css'
+
+const styles = {
+  container: {
+    width: '100%',
+    heigh: '100%',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    alignItems: 'center'
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
+        <div style={styles.container}>
+          <QuestionContainer />
+          <ExplorationContainer />
+        </div>
       </div>
     );
   }
